d82102b21cf5ff5ec776ae9d27db304f76b53211
niinii-KO <123786887+niinii-KO@users.noreply.github.com>
2024-04-08 00:24:30 +0900

    Update clinicalTrainingBot.py

--- clinicalTrainingBot.py
+++ clinicalTrainingBot.py
@@ -1,1 +1,15 @@
-aaaa
+#参考：https://qiita.com/UT_ITDaiku/items/48c8c75d35b2005e913d
+import requests
+
+line_notify_token = LINE_NOTIFY_TOKEN
+line_notify_api = 'https://notify-api.line.me/api/notify'
+message = ""
+
+def main(message):
+    print('送信メッセージ：' + message)
+    payload = {'message': message}
+    headers = {'Authorization': 'Bearer ' + line_notify_token}
+    requests.post(line_notify_api, data=payload, headers=headers)
+
+if __name__== '__main__':
+    main(message)
